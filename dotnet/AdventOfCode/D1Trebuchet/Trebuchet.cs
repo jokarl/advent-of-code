d00793365de3081e3b598b323b2f1e1f694ed24d
Johan Karlsson <johan.a.karlsson_c@sandvik.com>
2023-12-08 13:54:21 +0100

    Day 1 part 2

--- dotnet/AdventOfCode/D1Trebuchet/Trebuchet.cs
+++ dotnet/AdventOfCode/D1Trebuchet/Trebuchet.cs
@@ -2,12 +2,71 @@
 
 public class Trebuchet
 {
-    public static int Linq(string[] args)
+    public static int Solve(string[] args)
     {
         return args
             .Select(arg => arg.Where(char.IsDigit).ToArray())
             .Where(digits => digits.Length != 0)
             .Select(digits => int.Parse("" + digits.First() + digits.Last()))
             .Sum();
+    }
+
+    private static readonly Dictionary<string, int> Dict = new()
+    {
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 }
+    };
+
+    public static int Solve_part2(string[] args)
+    {
+        return args
+            .Select(Convert)
+            .Where(digits => digits.Length != 0)
+            .Select(digits => int.Parse("" + digits.First() + digits.Last()))
+            .Sum();
+    }
+
+    private static char[] Convert(string arg)
+    {
+        // Create a char array with the same length as the input string
+        // Keep the original indexes here, so we can insert the digits at the correct index
+        var chars = new char[arg.Length];
+
+        // First find all digits, insert them into the array with the index they were found at
+        for (var i = 0; i < arg.Length; i++)
+        {
+            if (char.IsDigit(arg[i]))
+            {
+                chars[i] = arg[i];
+            }
+        }
+
+        // Iterate all valid number strings that we can replace
+        foreach (var e in Dict)
+        {
+            // If the input string does not contain the number string, skip it
+            //if (!arg.Contains(e.Key)) continue;
+
+            // Find the first index
+            var index = arg.IndexOf(e.Key, StringComparison.Ordinal);
+            chars[index] = char.Parse(e.Value.ToString());
+            // Use the first index to keep progressing through the string
+            for (var i = index; i < arg.Length; i++)
+            {
+                var next = arg.IndexOf(e.Key, i, StringComparison.Ordinal);
+                if (next == -1) continue;
+                chars[next] = char.Parse(e.Value.ToString());
+            }
+        }
+
+        var convert = chars.Where(c => c != '\0').ToArray();
+        return convert;
     }
 }
--- dotnet/AdventOfCode/Tests/TrebuchetTests.cs
+++ dotnet/AdventOfCode/Tests/TrebuchetTests.cs
@@ -6,12 +6,30 @@ namespace Tests;
 public class TrebuchetTests
 {
     private const int ExpectedResult = 54304;
+    private const int ExpectedResultPart2 = 54418;
 
     [Fact]
-    public void Linq()
+    public void Solve()
     {
         var input = File.ReadLines("Data/Trebuchet.txt").ToArray();
-        var result = Trebuchet.Linq(input);
+        var result = Trebuchet.Solve(input);
         Assert.Equal(ExpectedResult, result);
+    }
+    
+    [Fact]
+    public void Solve_part2()
+    {
+        var input = File.ReadLines("Data/Trebuchet.txt").ToArray();
+        var result = Trebuchet.Solve_part2(input);
+        Assert.Equal(ExpectedResultPart2, result);
+    }
+
+    [Fact]
+    public void Part2_regex()
+    {
+        const string input = "sgeightwo3";
+        const string regex = @"(?:\d+)|(?=one)|(?=eight)";
+
+        var matches = Regex.Matches(input, regex);
     }
 }
